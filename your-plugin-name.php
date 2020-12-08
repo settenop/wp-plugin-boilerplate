c235827f4184380f9f16caede351161517140784
Settimo Augusto Vittorio <settenop@gmail.com>
2020-12-08 13:40:20 +0100

    first commit

---
+++ your-plugin-name.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: YOUR_PLUGIN_NAME
+ *
+ * @package YOUR_PLUGIN_NAME
+ */
+
+/**
+ * YOUR_PLUGIN_NAME main class, singleton.
+ */
+class YOUR_PLUGIN_NAME {
+
+	/**
+	 * Builds the plugin instance.
+	 */
+	private function __construct() { }
+
+	/**
+	 * Plugin instance.
+	 *
+	 * @var object $instance
+	 */
+	private static YOUR_PLUGIN_NAME $instance;
+
+	/**
+	 * Gets the plugin instance for the current request, if no instance is found then one is built.
+	 */
+	public static function get_instance() : YOUR_PLUGIN_NAME {
+		if ( ! isset( $instance ) ) {
+			$instance = new YOUR_PLUGIN_NAME();
+		}
+
+		return $instance;
+	}
+}
