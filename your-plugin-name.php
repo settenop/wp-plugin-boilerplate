bc4217fd604c7851f450286c936390e6edd7b310
Settimo Augusto Vittorio <settenop@gmail.com>
2021-03-27 19:23:31 +0100

    fix

--- your-plugin-name.php
+++ your-plugin-name.php
@@ -26,10 +26,10 @@ class YOUR_PLUGIN_NAME {
 	 * Gets the plugin instance for the current request, if no instance is found then one is built.
 	 */
 	public static function get_instance() : YOUR_PLUGIN_NAME {
-		if ( ! isset( $instance ) ) {
-			$instance = new YOUR_PLUGIN_NAME();
+		if ( ! isset( YOUR_PLUGIN_NAME::$instance ) ) {
+			YOUR_PLUGIN_NAME::$instance = new YOUR_PLUGIN_NAME();
 		}
 
-		return $instance;
+		return YOUR_PLUGIN_NAME::$instance;
 	}
 }
